80a9008bf4f4f4bca271818b3cc3cd09b8856d92
Tejas Kumthekar <tkumthekar@egencia.com>
2019-03-03 00:46:45 -0600

    Next, Prev youtube and spotify

--- tabs.js
+++ tabs.js
@@ -27,6 +27,58 @@ document.addEventListener("click", (e) => {
                 }
             }
         });
+    } else if (e.target.id === 'youtubeNext') {
+        chrome.tabs.query({
+        }, (tabs) => {
+            for (var tab of tabs) {
+                console.log(tab);
+                if (tab.url.includes('youtube')) {
+                    chrome.tabs.executeScript(tab.id, {
+                        code: 'document.getElementsByClassName("ytp-next-button")[0].click()'
+                    }, (result) => {
+                        console.log(result);
+                    });
+                }
+            }
+        });
+    } else if (e.target.id === 'youtubePrevious') {
+        chrome.tabs.query({
+        }, (tabs) => {
+            for (var tab of tabs) {
+                console.log(tab);
+                if (tab.url.includes('youtube')) {
+                    chrome.tabs.goBack(tab.id);
+                }
+            }
+        });
+    } else if (e.target.id === 'spotifyNext') {
+        chrome.tabs.query({
+        }, (tabs) => {
+            for (var tab of tabs) {
+                console.log(tab);
+                if (tab.url.includes('spotify')) {
+                    chrome.tabs.executeScript(tab.id, {
+                        code: 'document.getElementsByClassName("spoticon-skip-forward-16")[0].click();'
+                    }, (result) => {
+                        console.log(result);
+                    });
+                }
+            }
+        });
+    } else if (e.target.id === 'spotifyPrevious') {
+        chrome.tabs.query({
+        }, (tabs) => {
+            for (var tab of tabs) {
+                console.log(tab);
+                if (tab.url.includes('spotify')) {
+                    chrome.tabs.executeScript(tab.id, {
+                        code: 'document.getElementsByClassName("spoticon-skip-back-16")[0].click();'
+                    }, (result) => {
+                        console.log(result);
+                    });
+                }
+            }
+        });
     }
 
     e.preventDefault();
